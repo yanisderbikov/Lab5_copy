db0f6363e8bcdd9ab6fe2fc0be6f38cbd2b4a211
yanderbikovmail.ru <yanisderbikov@gmail.com>
2022-12-01 16:07:10 +0300

    There are half

--- lab5/main.c
+++ lab5/main.c
@@ -5,8 +5,8 @@
 //-----------------------------------------------------------------------------
 #include "C8051F360.h"                 // SFR declarations
 #include <string.h>
-#include <iostream>
-#include <string>
+//#include <iostream>
+//#include <string>
 
 //-----------------------------------------------------------------------------
 // Global CONSTANTS
@@ -19,8 +19,15 @@ sbit RS = P1^5;
 sbit RW = P1^6;
 sbit SE = P1^7; // E
 
+// at void read_code used
+char out;
+int ROW0;
+int ROW1;
+int ROW2;
+int ROW3;
 
-int i;
+char flag;
+
 long li;
 
 
@@ -28,15 +35,21 @@ long li;
 // Function PROTOTYPES
 //-----------------------------------------------------------------------------
 
-void Port_IO_Init();
-void writeCommand(char com);
-void writeChar(char sym);
+void write_Command(char com);
+void write_code(int sym);
+void write_sentence(unsigned char *STR);
 void displayInit(void);
+void read_code(void);
+
+
+void Port_IO_Init();
 void wait(long count);
 char getNum();
 char convert(int n);
 int pow2(int to);
 char AF(int rest);
+
+
 
 //-----------------------------------------------------------------------------
 // MAIN Routine
@@ -50,7 +63,7 @@ int main (){
 
     while(i < 3){                       // possible to
         char currentNum = getNum();
-        writeChar(currentNum);
+        write_code(currentNum);
         i++;
     }
     return 0;
@@ -58,7 +71,7 @@ int main (){
 
 
 // written commands
-void writeCommand(char com){
+void write_Command(char com){
     int i;
 
     P2 = com;
@@ -70,24 +83,74 @@ void writeCommand(char com){
     SE = 0;
     wait(10);
 }
-void writeChar(char sym){
-    int i;
-
-    P2 = sym;
+void write_code(int write_data){
+    P2 = write_data;
     RS = 1;
     RW = 0;
-    SE = 1;
 
-    wait(10);
+    wait(5);
+    SE = 1;
+    wait(5000);
+
     SE = 0;
-    wait(10);
+} //write symbol at screen
+void write_sentence(unsigned char *STR){
+    for (int n = 0; STR[n] != 0; n++) {
+        write_code(STR[n]);
+    }
+} // write a sentence
+void read_code(){
+    char tmp;
+    out = 'Z';
+    P2 = 0xff;
+    ROW0 = 0;
+    ROW1 = 0;
+    ROW2 = 0;
+    flag = 'Z';
+
+    tmp = P2;
+    wait(10000); // 15ms
+
+    // присвоим переменной flag одно из значений
+    // опрашиваем поочередно каждую строчку
+//    ---------A-------
+
+    if (!(tmp & 0x1)){
+        flag = 'A';
+    }
+
+//    ---------B-------
+    ROW0 = 1;
+    ROW1 = 0;
+    tmp = P2;
+
+    if (!(tmp & 0x1)){
+        flag = 'B';
+    }
+
+//    ---------C-------
+    ROW1 = 1;
+    ROW2 = 0;
+
+    if (!(tmp & 0x1)){
+        flag = 'C';
+    }
+
+//    ---------D-------
+    ROW2 = 1;
+    ROW3 = 0;
+    if (!(tmp & 0x1)){
+        flag = 'D';
+    }
 }
+
 char getNum(){
 //    здесь должна быть реализация кода, где берется и возвращяется значение с
 //    тыкнутой кнопки
 
     return 0;
-}
+}// unwritten
+
 
 // commands needs to
 void wait(long count){
@@ -98,26 +161,26 @@ void displayInit(void){ 			    // initialization screen
 
     wait(10000); //1000 : 10��	�������� ��������  ��������
 
-    writeCommand(0x3c);				// ������� ���� ������ 8 ��� 2 ������ ������� 5�10
+    write_Command(0x3c);				// ������� ���� ������ 8 ��� 2 ������ ������� 5�10
     wait(10000); //1000 : 10��
 
-    writeCommand(0x3c);
+    write_Command(0x3c);
     wait(10000); //1000 : 10��
 
-    writeCommand(0x3c);
+    write_Command(0x3c);
 
     wait(10000);
 
-    writeCommand(0x3c);
+    write_Command(0x3c);
     wait(10000);
-    writeCommand(0x08);			// ����� �����������: ����������� - ���������, ������ - ��������,
+    write_Command(0x08);			// ����� �����������: ����������� - ���������, ������ - ��������,
     // ������ � ���� ���������� ����� - ��������
     wait(10000);
-    writeCommand(0x01);			// ������� ������ ������
+    write_Command(0x01);			// ������� ������ ������
     wait(10000);
-    writeCommand(0x06);			// ����� �������� ���������� - �� ����������, ��� ������� ������
+    write_Command(0x06);			// ����� �������� ���������� - �� ����������, ��� ������� ������
     wait(10000);
-    writeCommand(0xC);
+    write_Command(0xC);
     wait(10000);
 }
 void Port_IO_Init(){
